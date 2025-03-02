517bee1ff36bdda9e49add648ce4f6a970e40474
Alex <alex2323official@gmail.com>
2025-03-02 22:49:49 +0100

    Add todo

--- app_frontend/src/assets/components/Header/Header.tsx
+++ app_frontend/src/assets/components/Header/Header.tsx
@@ -1,12 +1,21 @@
 import { googleLogout } from "@react-oauth/google";
 import { useNavigate } from "react-router";
+import { useEffect, useState } from "react";
 import "./Header.styles.css";
 const Header = () => {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    setIsLoggedIn(user !== null);
+  }, []);
+
   // Use this handler to logout from app
   const handleLogout = () => {
     googleLogout();
     localStorage.removeItem("user"); // Remove stored user data
+    setIsLoggedIn(false);
     navigate("/"); // Redirect to home
   };
   return (
@@ -33,11 +42,14 @@ const Header = () => {
               Ustawienia
             </a>
           </li>
-          <li className="header__li">
-            <button onClick={handleLogout} className="header__btn-logout">
-              Wyloguj
-            </button>
-          </li>
+          {/* Show logout button only if user is logged in */}
+          {isLoggedIn && (
+            <li className="header__li">
+              <button onClick={handleLogout} className="header__btn-logout">
+                Wyloguj
+              </button>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
